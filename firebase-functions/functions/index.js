b8491f68718b26ec46028dce6b3209cb1b225f5a
kubrom <kubrom.mulugheta@gmail.com>
2020-06-08 17:08:13 +0200

    added get address fun

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -4,6 +4,11 @@ const cors = require("cors");
 const admin = require("firebase-admin");
 admin.initializeApp();
 const app = express();
+
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+//var XMLHttpRequest = require("xhr2");
+//var xhr = new XMLHttpRequest();
+
 app.use(cors({ origin: true }));
 
 //ENDPOINT TO GET ALL IMAGES
@@ -20,3 +25,42 @@ app.get("/", async (req, res) => {
 });
 
 exports.GetImages = functions.https.onRequest(app);
+
+exports.createLocation = functions.firestore
+  .document(`/Images/{id}`)
+  .onCreate(async (snap, context) => {
+    const data = snap.data();
+
+    const latitude = data.latitude;
+    const longitude = data.longitude;
+    const location = getAddress(latitude, longitude);
+  });
+
+var request = new XMLHttpRequest();
+
+function getAddress(latitude, longitude) {
+  return new Promise(function (resolve, reject) {
+    var method = "GET";
+    var url =
+      "http://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+      latitude +
+      "," +
+      longitude +
+      "&sensor=true";
+    var async = true;
+
+    request.open(method, url, async);
+    request.onreadystatechange = function () {
+      if (request.readyState == 4) {
+        if (request.status == 200) {
+          var data = JSON.parse(request.responseText);
+          var address = data.results[0];
+          resolve(address);
+        } else {
+          reject(request.status);
+        }
+      }
+    };
+    request.send();
+  });
+}
